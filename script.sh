9576f35825d86bec7591b95cec4e25283652676f
GimhanAkalanke <55176434+GimhanAkalanke@users.noreply.github.com>
2019-09-16 21:50:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 LOGFILE="/var/log/DEV_OPS_training.log"
+GITPATH="/opt/DEV_OPS"
+GITUSER="GimhanAkalanke"
+GITEMAIL="myinbox.gm@gmail.com"
+GITPW="Vcs_1234"
+
+#####EXIT CODES#####
+##100### Changing to a directory failed
 
 echo "DEV_OPS_Trainng Log"  > ${LOGFILE}
 echo "==================="  >> ${LOGFILE}
@@ -16,21 +23,39 @@ echo "Installing GIT....."  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] Making ENV for GIT repository"  >> ${LOGFILE}
-mkdir /opt/DEV_OPS
-cd /opt/DEV_OPS
+  if [ ! -d "$GITPATH" ]
+  then
+      if mkdir -p $GITPATH
+      then
+        echo "[NORMAL] \"$GITPATH\" directory created" >> ${LOGFILE}
+      else
+        echo "[MAJOR] \"$GITPATH\" directory create failed" >> ${LOGFILE}
+      fi
+  else
+    echo "[WARNING] \"$GITPATH\" directory already exsists" >> ${LOGFILE}
+  fi
+  
+  if cd $GITPATH
+  then
+    echo "[NORMAL] Changed to $GITPATH"
+  else
+    echo "[CRITICAL] Changing to $GITPATH failed.. Exiting..100"
+    exit 100
+  fi
+  
 echo "[NORMAL] Making ENV for GIT repository Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] GIT repository configuration"  >> ${LOGFILE}
-git config --global user.name "GimhanAkalanke"
-git config --global user.email myinbox.gm@gmail.com
+git config --global user.name $GITUSER
+git config --global user.email $GITEMAIL
 git init
 git remote add origin https://github.com/GimhanAkalanke/DEV_OPS.git
 echo "[NORMAL] GIT repository configuration Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] Pulling sshd_config file"  >> ${LOGFILE}
-git pull https://GimhanAkalanke:Vcs_1234@github.com/GimhanAkalanke/DEV_OPS.git master
+git pull https://$GITUSER:$GITPW@github.com/GimhanAkalanke/DEV_OPS.git master
 echo "[NORMAL] Pulling sshd_config file Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
@@ -52,6 +77,16 @@ echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] Setting root password"  >> ${LOGFILE}
 echo "vcs@1234" | passwd --stdin root
 echo "[NORMAL] Setting root password Completed"  >> ${LOGFILE}
+
+echo "==================="  >> ${LOGFILE}
+echo "Installing Java 1.8....."  >> ${LOGFILE}
+  if yum install -y java-1.8*
+  then
+      echo "[NORMAL] Installing Java 1.8 Completed"  >> ${LOGFILE}
+      java -version >> ${LOGFILE}
+  else
+      echo "[WARNING] Installing Java 1.8 Failed" >> ${LOGFILE}
+  fi
 
 echo "==================="  >> ${LOGFILE}
 echo "===END OF SR======="  >> ${LOGFILE}
