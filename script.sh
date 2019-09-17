b7abf36a8023688932356784550935f09b738991
GimhanAkalanke <55176434+GimhanAkalanke@users.noreply.github.com>
2019-09-17 11:00:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,12 @@ GITPATH="/opt/DEV_OPS"
 GITUSER="GimhanAkalanke"
 GITEMAIL="myinbox.gm@gmail.com"
 GITPW="Vcs_1234"
+GITREPO="https://github.com/GimhanAkalanke/DEV_OPS.git"
+RTPW="vcs@1234"
 
 #####EXIT CODES#####
 ##100### Changing to a directory failed
+##101### Config file download from GIT failed
 
 echo "DEV_OPS_Trainng Log"  > ${LOGFILE}
 echo "==================="  >> ${LOGFILE}
@@ -47,21 +50,35 @@ echo "[NORMAL] Making ENV for GIT repository Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] GIT repository configuration"  >> ${LOGFILE}
-git config --global user.name $GITUSER
-git config --global user.email $GITEMAIL
-git init
-git remote add origin https://github.com/GimhanAkalanke/DEV_OPS.git
-echo "[NORMAL] GIT repository configuration Completed"  >> ${LOGFILE}
+git config --global user.name $GITUSER     ##No harm if failed at this point
+git config --global user.email $GITEMAIL   ##No harm if failed at this point
+  if git init
+  then
+    if git remote add origin $GITREPO
+    then
+      echo "[NORMAL] GIT repository configuration Completed"  >> ${LOGFILE}
+    else
+      echo "[MAJOR] GIT repository initializing Failed" >> ${LOGFILE}
+    fi
+  else
+    echo "[CRITICAL] GIT repository configuration Failed" >> ${LOGFILE}
+  fi
+
 
 echo "==================="  >> ${LOGFILE}
-echo "[NORMAL] Pulling sshd_config file"  >> ${LOGFILE}
-git pull https://$GITUSER:$GITPW@github.com/GimhanAkalanke/DEV_OPS.git master
-echo "[NORMAL] Pulling sshd_config file Completed"  >> ${LOGFILE}
+echo "[NORMAL] Pulling config files"  >> ${LOGFILE}
+  if git pull https://$GITUSER:$GITPW@github.com/GimhanAkalanke/DEV_OPS.git master
+  then
+    echo "[NORMAL] Pulling config files Completed"  >> ${LOGFILE}
+  else
+    echo "[CRITICAL] Pulling config files Failed.Cannot run further. Exiting....101" >> ${LOGFILE}
+    exit 101
+  fi
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] Replacing sshd_config file"  >> ${LOGFILE}
-rm -rf /etc/ssh/sshd_config
-cp -rf /opt/DEV_OPS/sshd_config /etc/ssh/sshd_config
+rm -f /etc/ssh/sshd_config
+cp -f /opt/DEV_OPS/sshd_config /etc/ssh/sshd_config
 echo "[NORMAL] Replacing sshd_config file Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
@@ -75,7 +92,7 @@ echo "[NORMAL] Restarting sshd service"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
 echo "[NORMAL] Setting root password"  >> ${LOGFILE}
-echo "vcs@1234" | passwd --stdin root
+echo "$RTPW" | passwd --stdin root
 echo "[NORMAL] Setting root password Completed"  >> ${LOGFILE}
 
 echo "==================="  >> ${LOGFILE}
@@ -86,6 +103,20 @@ echo "Installing Java 1.8....."  >> ${LOGFILE}
       java -version >> ${LOGFILE}
   else
       echo "[WARNING] Installing Java 1.8 Failed" >> ${LOGFILE}
+  fi
+
+echo "==================="  >> ${LOGFILE}
+echo "[NORMAL] Configuring Jenkins repo"  >> ${LOGFILE}
+  if wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+  then
+    if rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+    then
+      echo "[NORMAL] Configuring Jenkins repo Completed"  >> ${LOGFILE}
+    else
+      echo "[WARNING] Jenkins key import Failed"  >> ${LOGFILE}
+    fi
+  else
+    echo "[MAJOR] Configuring Jenkins repo Failed"  >> ${LOGFILE}
   fi
 
 echo "==================="  >> ${LOGFILE}
